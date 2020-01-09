ee3b16d3ab3b379c3debbb460d094d2dc9806539
Julio Lopez <julio+gh@kasten.io>
2020-01-09 15:40:54 -0800

    Manifest functions to support repair and purge

--- snapshot/gc/manifest.go
+++ snapshot/gc/manifest.go
@@ -98,3 +98,131 @@ func markManifestLabels() map[string]string {
 		manifest.TypeLabelKey: markManifestType,
 	}
 }
+
+func listMarkManifestsMeta(ctx context.Context, mgr *manifest.Manager) ([]*manifest.EntryMetadata, error) {
+	return mgr.Find(ctx, markManifestLabels())
+}
+
+func listMarkManifestsOlderThan(ctx context.Context, rep *repo.DirectRepository, minAge time.Duration) ([]MarkManifest, error) {
+	mgr := rep.Manifests
+	ageDate := rep.Time().Add(-minAge)
+
+	meta, err := listMarkManifestsMeta(ctx, mgr)
+	if err != nil {
+		return nil, err
+	}
+
+	var mms []MarkManifest
+
+	for _, m := range meta {
+		if m.ModTime.After(ageDate) {
+			continue // ignore recent entries
+		}
+
+		var man MarkManifest
+		if _, err := mgr.Get(ctx, m.ID, &man); err != nil {
+			if err == manifest.ErrNotFound {
+				continue
+			}
+
+			return nil, err
+		}
+
+		if man.EndTime.After(ageDate) {
+			continue
+		}
+
+		man.ID = m.ID
+		mms = append(mms, man)
+	}
+
+	return mms, nil
+}
+
+func getMarkDetails(ctx context.Context, cm *content.Manager, detailsID content.ID) (MarkDetails, error) {
+	var details MarkDetails
+
+	b, err := cm.GetContent(ctx, detailsID)
+	if err != nil {
+		return details, err
+	}
+
+	err = json.Unmarshal(b, &details)
+
+	return details, err
+}
+
+type empty struct{}
+
+type manifestIDSet map[manifest.ID]empty
+
+func toManifestIDSet(ids []manifest.ID) manifestIDSet {
+	s := make(manifestIDSet, len(ids))
+
+	return s.addAll(ids...)
+}
+
+func (s manifestIDSet) addAll(ids ...manifest.ID) manifestIDSet {
+	for _, id := range ids {
+		s.put(id)
+	}
+
+	return s
+}
+
+func (s manifestIDSet) put(id manifest.ID) {
+	s[id] = empty{}
+}
+
+func (s manifestIDSet) has(id manifest.ID) bool {
+	_, ok := s[id]
+
+	return ok
+}
+
+// add adds the elements in b to s. It is equivalent to `s = s U b`
+func (s manifestIDSet) add(b manifestIDSet) manifestIDSet {
+	for id := range b {
+		s.put(id)
+	}
+
+	return s
+}
+
+// subtract removes the elements in b from s. It is equivalent to `s = s - b`
+func (s manifestIDSet) subtract(b manifestIDSet) manifestIDSet {
+	for id := range b {
+		delete(s, id)
+	}
+
+	return s
+}
+
+// diffS is a convenience helper that is the "slice-version" of diff.
+func (s manifestIDSet) diffS(ids []manifest.ID) manifestIDSet {
+	return s.diff(toManifestIDSet(ids))
+}
+
+// diff returns a new set `d = s - b` without modifying `s`.
+func (s manifestIDSet) diff(b manifestIDSet) manifestIDSet {
+	d := manifestIDSet{}
+
+	for id := range s {
+		if !b.has(id) {
+			d.put(id)
+		}
+	}
+
+	return d
+}
+
+// return a slice with the contents of s
+func (s manifestIDSet) slice() []manifest.ID {
+	ids := make([]manifest.ID, 0, len(s))
+
+	for id := range s {
+		ids = append(ids, id)
+	}
+
+	return ids
+}
