45d01b36a08db60b4f2bd867c501f2b3ac1c774f
Nick Wright <nick@kasten.io>
2020-01-22 18:25:58 -0800

    Adding options

---
+++ tests/tools/fio/options.go
@@ -0,0 +1,13 @@
+package fio
+
+// Options are flags to be set when running fio
+type Options map[string]string
+
+// Merge will merge two Options, overwriting common option keys
+// with the incoming option values. Returns the merged result
+func (opt Options) Merge(otherOpt Options) map[string]string {
+	for k, v := range otherOpt {
+		opt[k] = v
+	}
+	return opt
+}
