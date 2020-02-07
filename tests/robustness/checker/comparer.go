2c02497d265fce131f091108f594e7fdb1edde48
Nick Wright <nick@kasten.io>
2020-02-07 14:54:33 -0800

    Add comparer interface and fswalker implementation
    Add comparer interface which gathers data on a path and
    compares that data to a new path, returning error if the path
    differs in any way from the input data. The details of what
    constitutes a difference is left to the implementation.
    
    FSWalker implementation uses Walk and Report to do the data
    gathering and comparison.

---
+++ tests/robustness/checker/comparer.go
@@ -0,0 +1,13 @@
+package checker
+
+import (
+	"context"
+	"io"
+)
+
+// Comparer describes an interface that gathers state data on a provided
+// path, and compares that data to the state on another path.
+type Comparer interface {
+	Gather(ctx context.Context, path string) ([]byte, error)
+	Compare(ctx context.Context, path string, data []byte, reportOut io.Writer) error
+}
