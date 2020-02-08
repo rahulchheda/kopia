c5e3ba23a6732097fd57733484d378bfc9424f07
Nick Wright <nick@kasten.io>
2020-02-07 16:47:33 -0800

    Add snaphot storer interface and kopia implementation
    Snapstore package has two interfaces.
    Storer describes the ability to store and retrieve a buffer
    of metadata associated with a string key.
    DataPersister describes the ability to flush a store of
    metadata to a repository and load it again.
    
    Add the kopia metadata repository implementation, which
    implements the Storer and DataPersister interfaces.

---
+++ tests/robustness/snapstore/simple.go
@@ -0,0 +1,71 @@
+package snapstore
+
+import "sync"
+
+var _ Storer = &Simple{}
+
+// Simple is a snapstore implementation that stores
+// snapshot metadata as a byte slice in a map in memory
+type Simple struct {
+	s map[string][]byte
+	l *sync.Mutex
+}
+
+// NewSimple instantiates a new Simple snapstore and
+// returns its pointer
+func NewSimple() *Simple {
+	return &Simple{
+		s: make(map[string][]byte),
+		l: new(sync.Mutex),
+	}
+}
+
+// Store implements the Storer interface Store method
+func (store *Simple) Store(key string, val []byte) error {
+	buf := make([]byte, len(val))
+	_ = copy(buf, val)
+
+	store.l.Lock()
+	defer store.l.Unlock()
+
+	store.s[key] = buf
+
+	return nil
+}
+
+// Load implements the Storer interface Load method
+func (store *Simple) Load(key string) ([]byte, error) {
+	store.l.Lock()
+	defer store.l.Unlock()
+
+	if buf, found := store.s[key]; found {
+		retBuf := make([]byte, len(buf))
+		_ = copy(retBuf, buf)
+
+		return retBuf, nil
+	}
+
+	return nil, nil
+}
+
+// Delete implements the Storer interface Delete method
+func (store *Simple) Delete(key string) {
+	store.l.Lock()
+	defer store.l.Unlock()
+
+	delete(store.s, key)
+}
+
+// GetKeys implements the Storer interface GetKeys method
+func (store *Simple) GetKeys() []string {
+	ret := make([]string, 0, len(store.s))
+
+	store.l.Lock()
+	defer store.l.Unlock()
+
+	for k := range store.s {
+		ret = append(ret, k)
+	}
+
+	return ret
+}
---
+++ tests/robustness/snapstore/snapstore.go
@@ -0,0 +1,22 @@
+// Package snapstore describes entities that can accept
+// arbitrary metadata and flush it to a persistent repository.
+package snapstore
+
+import "github.com/kopia/kopia/tests/robustness/snapif"
+
+// Storer describes the ability to store and retrieve
+// a buffer of metadata, indexed by a string key.
+type Storer interface {
+	Store(key string, val []byte) error
+	Load(key string) ([]byte, error)
+	Delete(key string)
+	GetKeys() []string
+}
+
+// DataPersister describes the ability to flush metadata
+// to, and load it again, from a repository.
+type DataPersister interface {
+	snapif.RepoManager
+	LoadMetadata() error
+	FlushMetadata() error
+}
