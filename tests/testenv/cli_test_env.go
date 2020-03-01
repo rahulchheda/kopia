5dd8dcaa30c0416a6a81e6d9effac82f9e3dca9c
Nick <nwright970@gmail.com>
2020-03-01 19:14:54 +0000

    Use a bytes.Buffer to capture stderr
    Suggest to use a bytes.Buffer to capture stderr, let
    the exec package handle the stderr pipe behavior.

--- tests/testenv/cli_test_env.go
+++ tests/testenv/cli_test_env.go
@@ -3,6 +3,7 @@ package testenv
 
 import (
 	"bufio"
+	"bytes"
 	cryptorand "crypto/rand"
 	"encoding/hex"
 	"fmt"
@@ -14,7 +15,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"sync"
 	"testing"
 	"time"
 
@@ -207,29 +207,14 @@ func (e *CLITest) Run(t *testing.T, args ...string) (stdout, stderr []string, er
 	c := exec.Command(e.Exe, cmdArgs...)
 	c.Env = append(os.Environ(), e.environment...)
 
-	stderrPipe, err := c.StderrPipe()
-	if err != nil {
-		t.Fatalf("can't set up stderr pipe reader")
-	}
-
-	var errOut []byte
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-
-		errOut, err = ioutil.ReadAll(stderrPipe)
-	}()
+	errOut := &bytes.Buffer{}
+	c.Stderr = errOut
 
 	o, err := c.Output()
 
-	wg.Wait()
-	t.Logf("finished 'kopia %v' with err=%v and output:\n%v\nstderr:\n%v\n", strings.Join(args, " "), err, trimOutput(string(o)), trimOutput(string(errOut)))
+	t.Logf("finished 'kopia %v' with err=%v and output:\n%v\nstderr:\n%v\n", strings.Join(args, " "), err, trimOutput(string(o)), trimOutput(errOut.String()))
 
-	return splitLines(string(o)), splitLines(string(errOut)), err
+	return splitLines(string(o)), splitLines(errOut.String()), err
 }
 
 func trimOutput(s string) string {
